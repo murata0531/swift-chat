a3707b05fdb5d1c77e8d9a201e176eab4d76e244
murata0531 <56902467+murata0531@users.noreply.github.com>
2021-08-07 22:14:43 +0900

    ログインしていなければログイン画面に飛ばす
    おk

--- swift-chat/ChatRoom.swift
+++ swift-chat/ChatRoom.swift
@@ -6,15 +6,37 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
 struct ChatRoom: View {
+    
+    @State var isActiveLogin = false
+
+    let room_id:String
+    @State private var isInActiveLogin = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            NavigationLink(
+                destination: ContentView(),
+                isActive: $isInActiveLogin){
+                EmptyView()
+            }
+        }
+        .onAppear {
+            
+            if Auth.auth().currentUser != nil {
+              // User is signed in.
+              // ...
+            } else {
+              // No user is signed in.
+              // ...
+                self.isInActiveLogin = true
+            }
+        }
     }
 }
 
-struct ChatRoom_Previews: PreviewProvider {
-    static var previews: some View {
-        ChatRoom()
-    }
-}
