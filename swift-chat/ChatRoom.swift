bd0965294649e97633be6f849b181b6d8853e139
murata0531 <56902467+murata0531@users.noreply.github.com>
2021-08-13 23:55:47 +0900

    データの読み取りと書き込み処理を追加

--- swift-chat/ChatRoom.swift
+++ swift-chat/ChatRoom.swift
@@ -12,10 +12,13 @@ import FirebaseDatabase
 
 struct ChatRoom: View {
     
-    @State var isActiveLogin = false
+    @State private var isActiveLogin = false
     @State private var isInActiveLogin = false
-    @State var rooms = [String]()
+    @State private var rooms = [String]()
     @State private var message = ""
+    @State private var enable: Bool = false
+    @State private var color = Color.gray
+    @State private var user_id:String = ""
     
     let room_id:String
     let db = Firestore.firestore()
@@ -29,9 +32,7 @@ struct ChatRoom: View {
             }
             
             List(rooms, id: \.self) { item in
-                NavigationLink(destination: ChatRoom(room_id: String(item))) {
-                    Text(item)
-                }
+                Text(item)
             }
             
             Spacer()
@@ -39,26 +40,51 @@ struct ChatRoom: View {
             HStack {
                 TextField("RoundedBorderTextFieldStyle", text: $message)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .onChange(of: message){ value in
+                        if value == "" {
+                            self.enable = false
+                            self.color = Color.gray
+                        } else {
+                            self.enable = true
+                            self.color = Color.blue
+                        }
+                    }
                 
                 Button(action: {
-                    print("a")
+                    
+                    let dt = Date()
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "yMMMdHms", options: 0, locale: Locale(identifier: "ja_JP"))
+                    let room = dateFormatter.string(from: dt)
+                    let data: [String: Any] = ["uid": self.user_id, "time": room, "message": self.message]
+                    
+                    db.collection("chat_data").document(room_id).setData(data, merge: true){ err in
+                        if let err = err {
+                            print("Error writing document: \(err)")
+                        } else {
+                            self.message = ""
+                        }
+                    }
                 }){
-                    Text("送信")
+                    Text("送信").foregroundColor(Color.white)
                 }
+                .disabled(!enable)
+                .background(self.color)
             }
         }
         .onAppear {
             
             if Auth.auth().currentUser != nil {
               // User is signed in.
-              // ...
+                let user = Auth.auth().currentUser
+                if let user = user {
+                    self.user_id = user.uid
+                }
             } else {
               // No user is signed in.
-              // ...
                 self.isInActiveLogin = true
             }
             
-            rooms.removeAll()
             db.collection("chat_data").document(room_id)
                 .addSnapshotListener { documentSnapshot, error in
                   guard let document = documentSnapshot else {
@@ -70,6 +96,7 @@ struct ChatRoom: View {
                     return
                   }
                     rooms.append(String(data.count))
+                    print("Current data: \(data)")
                 }
         }
     }
