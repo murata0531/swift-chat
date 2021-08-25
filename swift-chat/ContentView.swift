02d287bfbbd003597633b0d696e436d0945a6f7d
murata0531 <56902467+murata0531@users.noreply.github.com>
2021-08-25 23:00:20 +0900

    ルートであるログイン画面の戻るボタンを非表示に

--- swift-chat/ContentView.swift
+++ swift-chat/ContentView.swift
@@ -66,6 +66,7 @@ struct ContentView: View {
                 }
                 Spacer().frame(width:50)
             }
+            .navigationBarBackButtonHidden(true)
             .onAppear {
                 if Auth.auth().currentUser != nil {
                     self.isActiveHome = true
